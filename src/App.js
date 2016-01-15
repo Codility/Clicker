92460f547c51a34b8d804cc7baa7a2bf20755851
Jakub_Swiatkowski <kuba@codility.com>
2016-01-15 16:44:59 +0000

    Set initial counter to 0

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { counter : 1, nick : null,
+    this.state = { counter : 0, nick : null,
       users : { 'Jakub' : 50, 'Kos' : 50, 'Marcin' : 60 },
       speed : 0,
       start_time : null

81508130fb09083a01a5f99429a221d61a8201ee
Jakub_Swiatkowski <kuba@codility.com>
2016-01-15 12:45:39 +0000

    Login screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { counter : 1, nick : null }
+  }
   render() {
+    if (this.state.nick === null) {
+      return <LoginForm/>
+    }
+
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <h1>Counter: {this.state.counter }.</h1>
+        <button onClick={this.increment.bind(this)}> Increment </button>
+      </div>
     );
   }
+
+  increment() {
+    this.setState({ counter : this.state.counter + 1 })
+  }
+}
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <div className="form-group">
+        <h1>Welcome</h1>
+        <label>Name:</label>
+        <input ref="nick" className="form-control"/>
+        <br/>
+        <button className="btn btn-primary" onClick={this.login.bind(this)}>Login</button>
+      </div>
+    )
+  }
+  login() {
+  }
+
 }
